10a007c0e93e1898369ac0e193c89ae350eeed47
Daniel Cohen <danielcohen@daniels-mbp.home>
2022-05-18 15:24:45 +0300

    skills refactor

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-// app.set("view engine", "html");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
@@ -13,15 +12,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Serving on port ${port}`);
 });
-
-// Can also be included with a regular script tag
-// const options = {
-//   strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
-//   typeSpeed: 40,
-// };
-// const typed = new Typed(".element", options);
-
-// var typed = new Typed(".element", {
-//   // Waits 1000ms after typing "First"
-//   strings: ["First ^1000 sentence.", "Second sentence."],
-// });
